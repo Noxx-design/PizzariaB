092fbbc8230acda70db0a66d02ee221593826a50
Noxx-design <rm94529@estudante.fieb.edu.br>
2025-09-23 07:11:36 -0300

    Add files via upload

--- pizzariab/src/main/java/com/itb/inf2bm/pizzariab/controller/ProdutoController.java
+++ pizzariab/src/main/java/com/itb/inf2bm/pizzariab/controller/ProdutoController.java
@@ -2,33 +2,44 @@ package com.itb.inf2bm.pizzariab.controller;
 
 
 import com.itb.inf2bm.pizzariab.model.entity.Produto;
+import com.itb.inf2bm.pizzariab.model.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+/*
+ @RequestBody   : Corpo da Requisição ( Re)
+
+*/
+
 @RestController
 @RequestMapping("/api/v1/produto")
 public class ProdutoController {
 
-    List<Produto> produtos = new ArrayList<Produto>();
+    @Autowired
+    private ProdutoService produtoService;
 
+    // Listar todos os produtos
     @GetMapping
-    public List<Produto> findAll() {
+    public ResponseEntity<List<Produto>> findAll() {
+        return ResponseEntity.ok(produtoService.findAll());
 
-        Produto p1 = new Produto();
-        p1.setId(java.lang.Long.valueOf(1));
-        p1.setNome("Pizza de Calabresa");
-        p1.setDescricao("Pizza de Calabresa com bastante cebola");
-        p1.setValorCompra(49.90);
-        p1.setValorVenda(49.90);
-        p1.setQuantidadeEstoque(999);
-        p1.setCodStatus(false);
-
-        produtos.add(p1);
-        return produtos;
     }
+
+    // Salvar Produto
+    public ResponseEntity<Produto> save(@RequestBody Produto produto) {
+        Produto novo = produtoService.save(produto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novo);
+    }
+
 
 }
