09440f91b45623a720c049a3dac65b54d1273770
Noxx-design <rm94529@estudante.fieb.edu.br>
2025-09-30 11:27:44 -0300

    Add files via upload

--- pizzariab/src/main/java/com/itb/inf2bm/pizzariab/controller/ProdutoController.java
+++ pizzariab/src/main/java/com/itb/inf2bm/pizzariab/controller/ProdutoController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +33,7 @@ public class ProdutoController {
     }
 
     // Salvar Produto
+    @PostMapping
     public ResponseEntity<Produto> save(@RequestBody Produto produto) {
         Produto novo = produtoService.save(produto);
         return ResponseEntity.status(HttpStatus.CREATED).body(novo);
